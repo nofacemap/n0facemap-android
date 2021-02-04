27bf6846a4f7fc53e755df811e005ffeb1cdba80
Paxana <Veganbikepunk@gmail.com>
2021-02-03 16:57:00 -0800

    some refactoring, added auth libraries and a couple of buttons for auth and leaderboard

--- app/src/main/java/com/example/unsurvgame/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/unsurvgame/ui/main/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.unsurvgame.ui.main
 
 import android.Manifest.permission.*
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
 import android.graphics.Color
@@ -12,11 +14,15 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProviders
 import com.example.unsurvgame.R
 import com.example.unsurvgame.models.*
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import com.mapbox.android.core.location.LocationEngineCallback
 import com.mapbox.android.core.location.LocationEngineProvider
@@ -45,221 +51,222 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mapboxMap: MapboxMap
-    private var geoSource: GeoJsonSource? = null
-    private lateinit var styledMap: Style
-    private var lastLatLng = LatLng(0.0, 0.0)
-    private lateinit var geoObj: GeoJSON
+  private lateinit var mapboxMap: MapboxMap
+  private var geoSource: GeoJsonSource? = null
+  private lateinit var styledMap: Style
+  private var lastLatLng = LatLng(0.0, 0.0)
+  private lateinit var geoObj: GeoJSON
 
-    companion object {
-        private const val DROPPED_MARKER_LAYER_ID = "DROPPED_MARKER_LAYER_ID"
+  companion object {
+    private const val DROPPED_MARKER_LAYER_ID = "DROPPED_MARKER_LAYER_ID"
+    private const val RC_SIGN_IN = 420
+  }
+
+  private val vm: MainActivityVM by lazy {
+    ViewModelProviders.of(this).get(MainActivityVM::class.java)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    if (Firebase.auth.currentUser == null) {
+      val providers = arrayListOf(
+        AuthUI.IdpConfig.EmailBuilder().build(),
+        AuthUI.IdpConfig.GoogleBuilder().build())
+
+      val resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+          val data: Intent? = result.data
+
+        }
+        else{
+          Toast.makeText(this, "You must be logged in to contribute and play.", Toast.LENGTH_LONG).show()
+        }
+      }
+
+      val loginActivity = AuthUI.getInstance()
+        .createSignInIntentBuilder()
+        .setAvailableProviders(providers)
+        .build()
+
+      resultLauncher.launch(loginActivity)
     }
 
-    private val vm: MainActivityVM by lazy {
-        ViewModelProviders.of(this).get(MainActivityVM::class.java)
-    }
+    Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+    setContentView(R.layout.activity_main)
+    Logger.addLogAdapter(AndroidLogAdapter())
+    runOncePermissionGranted(savedInstanceState)
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
-        setContentView(R.layout.activity_main)
-        Logger.addLogAdapter(AndroidLogAdapter())
-        runOncePermissionGranted(savedInstanceState)
+  }
 
-    }
+  private fun runOncePermissionGranted(savedInstanceState: Bundle?) {
+    map.onCreate(savedInstanceState)
+    map.getMapAsync(this)
+  }
 
-    private fun runOncePermissionGranted(savedInstanceState: Bundle?) {
-        map.onCreate(savedInstanceState)
-        map.getMapAsync(this)
-    }
+  override fun onMapReady(mapboxMap: MapboxMap) {
+    this.mapboxMap = mapboxMap
 
-    override fun onMapReady(mapboxMap: MapboxMap) {
-        this.mapboxMap = mapboxMap
+    mapboxMap.setStyle(
+      Style.Builder().fromUri("mapbox://styles/paxblueribbon/ckk1k54ln0x0u17o1l8zjblz8")
+    ) { styledMap ->
+      vm.geoJSON.observe(this, {
+        if (!this::geoObj.isInitialized) {
+          geoObj = it
+          val asJson = Gson().toJson(it)
 
-        mapboxMap.setStyle(
-            Style.Builder().fromUri("mapbox://styles/paxblueribbon/ckk1k54ln0x0u17o1l8zjblz8")
-        ) { styledMap ->
-            vm.geoJSON.observe(this, {
-                if (!this::geoObj.isInitialized) {
-                    geoObj = it
-                    val asJson = Gson().toJson(it)
-
-                    geoSource = GeoJsonSource("fbgeo", asJson)
-                    val symLayer = SymbolLayer("circleLayer", "fbgeo")
-                    styledMap.addSource(geoSource!!)
-                    styledMap.addImage(
-                        "dronecam",
-                        BitmapFactory.decodeResource(resources, R.drawable.dronecam)
-                    )
-                    styledMap.addLayer(
-                        symLayer.withProperties(
-                            iconImage("dronecam")
-                        )
-                    )
-                } else {
-                    val asJson = Gson().toJson(it)
-                    geoSource!!.setGeoJson(asJson)
-                }
-            })
-
-            vm.getGeoJson()
-
-            this.styledMap = styledMap
-            when (PackageManager.PERMISSION_GRANTED) {
-                ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) -> {
-                    enableLocationComponent()
-                }
-                else -> {
-                    requestPermissions(arrayOf(ACCESS_FINE_LOCATION), 1312)
-                }
-            }
-
-            val hoveringMarker = ImageView(this@MainActivity)
-            hoveringMarker.setImageResource(R.drawable.ic_iconfinder_sed_21_2231967)
-            val params = FrameLayout.LayoutParams(
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT,
-                Gravity.CENTER
+          geoSource = GeoJsonSource("fbgeo", asJson)
+          val symLayer = SymbolLayer("circleLayer", "fbgeo")
+          styledMap.addSource(geoSource!!)
+          styledMap.addImage(
+            "dronecam",
+            BitmapFactory.decodeResource(resources, R.drawable.dronecam)
+          )
+          styledMap.addLayer(
+            symLayer.withProperties(
+              iconImage("dronecam")
             )
-            hoveringMarker.layoutParams = params
-            map.addView(hoveringMarker)
-
-//            initDroppedMarker(styledMap)
-
-            selectLocButton.setOnClickListener {
-                val mapTargetLatLng: LatLng = mapboxMap.cameraPosition.target
-                vm.addPointToDB(
-                    Point.fromLngLat(
-                        mapTargetLatLng.longitude,
-                        mapTargetLatLng.latitude
-                    )
-                )
-            }
-
+          )
+        } else {
+          val asJson = Gson().toJson(it)
+          geoSource!!.setGeoJson(asJson)
         }
-    }
+      })
 
-//    private fun initDroppedMarker(loadedMapStyle: Style) {
-//        loadedMapStyle.addImage(
-//            "dropped-icon-image",
-//            BitmapFactory.decodeResource(resources, R.drawable.gpspng)
-//        )
-//        loadedMapStyle.addSource(GeoJsonSource("dropped-marker-source-id"))
-//        loadedMapStyle.addLayer(
-//            SymbolLayer(
-//                DROPPED_MARKER_LAYER_ID,
-//                "dropped-marker-source-id"
-//            ).withProperties(
-//                iconImage("dropped-icon-image"),
-//                visibility(NONE),
-//                iconAllowOverlap(true),
-//                iconIgnorePlacement(true)
-//            )
-//        )
-//    }
+      vm.getGeoJson()
 
-    @SuppressLint("MissingPermission")
-    private fun enableLocationComponent() {
-        if (PermissionsManager.areLocationPermissionsGranted(this)) {
-
-            val customLocationComponentOptions = LocationComponentOptions.builder(this)
-                .trackingGesturesManagement(true)
-                .foregroundDrawable(R.drawable.gpspin)
-                .backgroundTintColor(Color.TRANSPARENT)
-                .accuracyColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
-                .build()
-
-            val locationComponentActivationOptions =
-                LocationComponentActivationOptions.builder(this, styledMap)
-                    .locationComponentOptions(customLocationComponentOptions)
-                    .build()
-
-            mapboxMap.locationComponent.apply {
-                activateLocationComponent(locationComponentActivationOptions)
-                isLocationComponentEnabled = true
-                cameraMode = CameraMode.TRACKING
-                renderMode = RenderMode.COMPASS
-            }
-
-            if (mapboxMap.locationComponent.lastKnownLocation != null) {
-                val latLng = LatLng(
-                    mapboxMap.locationComponent.lastKnownLocation!!.latitude,
-                    mapboxMap.locationComponent.lastKnownLocation!!.longitude
-                )
-                val position = CameraPosition.Builder()
-                    .target(latLng)
-                    .zoom(18.0)
-                    .build()
-                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position))
-            }
-
-            initLocationEngine(mapboxMap)
+      this.styledMap = styledMap
+      when (PackageManager.PERMISSION_GRANTED) {
+        ContextCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) -> {
+          enableLocationComponent()
         }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun initLocationEngine(mapboxMap: MapboxMap) {
-        val locationEngine = LocationEngineProvider.getBestLocationEngine(this)
-        val request = LocationEngineRequest.Builder(1000L)
-            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
-            .setMaxWaitTime(5000L).build()
-        val callback = object : LocationEngineCallback<LocationEngineResult> {
-            override fun onSuccess(result: LocationEngineResult?) {
-                if (result != null && result.lastLocation != null) {
-                    val location = result.lastLocation
-                    val latLng = LatLng(location!!.latitude, location.longitude)
-                    if (lastLatLng != latLng) {
-                        lastLatLng = latLng
-                        val position = CameraPosition.Builder()
-                            .target(latLng)
-                            .zoom(18.0)
-                            .build()
-                        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position))
-                    }
-                }
-            }
-
-            override fun onFailure(exception: Exception) {
-                throw exception
-            }
-
+        else -> {
+          requestPermissions(arrayOf(ACCESS_FINE_LOCATION), 1312)
         }
+      }
 
-        locationEngine.requestLocationUpdates(request, callback, Looper.getMainLooper())
+      val hoveringMarker = ImageView(this@MainActivity)
+      hoveringMarker.setImageResource(R.drawable.ic_iconfinder_sed_21_2231967)
+      val params = FrameLayout.LayoutParams(
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        ViewGroup.LayoutParams.WRAP_CONTENT,
+        Gravity.CENTER
+      )
+      hoveringMarker.layoutParams = params
+      map.addView(hoveringMarker)
+
+      selectLocButton.setOnClickListener {
+        val mapTargetLatLng: LatLng = mapboxMap.cameraPosition.target
+        vm.addPointToDB(
+          Point.fromLngLat(
+            mapTargetLatLng.longitude,
+            mapTargetLatLng.latitude
+          )
+        )
+      }
+
     }
+  }
 
-    override fun onResume() {
-        super.onResume()
-        map.onResume()
+  @SuppressLint("MissingPermission")
+  private fun enableLocationComponent() {
+    if (PermissionsManager.areLocationPermissionsGranted(this)) {
+
+      val customLocationComponentOptions = LocationComponentOptions.builder(this)
+        .trackingGesturesManagement(true)
+        .foregroundDrawable(R.drawable.gpspin)
+        .backgroundTintColor(Color.TRANSPARENT)
+        .accuracyColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        .build()
+
+      val locationComponentActivationOptions =
+        LocationComponentActivationOptions.builder(this, styledMap)
+          .locationComponentOptions(customLocationComponentOptions)
+          .build()
+
+      mapboxMap.locationComponent.apply {
+        activateLocationComponent(locationComponentActivationOptions)
+        isLocationComponentEnabled = true
+        cameraMode = CameraMode.TRACKING
+        renderMode = RenderMode.COMPASS
+      }
+
+      if (mapboxMap.locationComponent.lastKnownLocation != null) {
+        val latLng = LatLng(
+          mapboxMap.locationComponent.lastKnownLocation!!.latitude,
+          mapboxMap.locationComponent.lastKnownLocation!!.longitude
+        )
+        val position = CameraPosition.Builder()
+          .target(latLng)
+          .zoom(18.0)
+          .build()
+        mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position))
+      }
+      initLocationEngine(mapboxMap)
     }
+  }
 
-    override fun onPause() {
-        super.onPause()
-        map.onPause()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            1312 -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    enableLocationComponent()
-                } else {
-                    Toast.makeText(
-                        this,
-                        "You've rejected location permissions.  We won't be able to get your location unless you enable them.",
-                        Toast.LENGTH_LONG
-                    ).show()
-                }
-                return
-            }
-            else -> {
-                //ignore all other requests
-            }
+  @SuppressLint("MissingPermission")
+  private fun initLocationEngine(mapboxMap: MapboxMap) {
+    val locationEngine = LocationEngineProvider.getBestLocationEngine(this)
+    val request = LocationEngineRequest.Builder(1000L)
+      .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+      .setMaxWaitTime(5000L).build()
+    val callback = object : LocationEngineCallback<LocationEngineResult> {
+      override fun onSuccess(result: LocationEngineResult?) {
+        if (result != null && result.lastLocation != null) {
+          val location = result.lastLocation
+          val latLng = LatLng(location!!.latitude, location.longitude)
+          if (lastLatLng != latLng) {
+            lastLatLng = latLng
+            val position = CameraPosition.Builder()
+              .target(latLng)
+              .zoom(18.0)
+              .build()
+            mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position))
+          }
         }
+      }
+
+      override fun onFailure(exception: Exception) {
+        throw exception
+      }
     }
+    locationEngine.requestLocationUpdates(request, callback, Looper.getMainLooper())
+  }
+
+  override fun onResume() {
+    super.onResume()
+    map.onResume()
+  }
+
+  override fun onPause() {
+    super.onPause()
+    map.onPause()
+  }
+
+  override fun onRequestPermissionsResult(
+    requestCode: Int,
+    permissions: Array<out String>,
+    grantResults: IntArray
+  ) {
+    super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    when (requestCode) {
+      1312 -> {
+        if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+          enableLocationComponent()
+        } else {
+          Toast.makeText(
+            this,
+            "You've rejected location permissions.  We won't be able to get your location unless you enable them.",
+            Toast.LENGTH_LONG
+          ).show()
+        }
+        return
+      }
+      else -> {
+        //ignore all other requests
+      }
+    }
+  }
 }
--- app/src/main/java/com/example/unsurvgame/ui/main/MainActivityVM.kt
+++ app/src/main/java/com/example/unsurvgame/ui/main/MainActivityVM.kt
@@ -1,11 +1,16 @@
 package com.example.unsurvgame.ui.main
 
+import android.app.Activity
 import android.app.Application
+import android.content.Intent
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.unsurvgame.interactors.FBInteractorImpl
 import com.example.unsurvgame.models.FBGeoJSON
 import com.example.unsurvgame.models.GeoJSON
+import com.firebase.ui.auth.AuthUI
 import com.mapbox.geojson.Point
 import com.orhanobut.logger.Logger
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -14,47 +19,48 @@ import io.reactivex.observers.DisposableObserver
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
 
-class MainActivityVM(app : Application) : AndroidViewModel(app)  {
-    private val disposer = CompositeDisposable()
+class MainActivityVM(app: Application) : AndroidViewModel(app) {
+  private val disposer = CompositeDisposable()
 
-    private val fbInteractor = FBInteractorImpl()
+  private val fbInteractor = FBInteractorImpl()
 
-    val geoJSON = MutableLiveData<GeoJSON>()
+  val geoJSON = MutableLiveData<GeoJSON>()
 
-    fun getGeoJson() {
-        disposer.add(
-            fbInteractor.getGeoJson().subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object : DisposableObserver<FBGeoJSON>(){
-                    override fun onNext(t: FBGeoJSON) {
-                        val converted = GeoJSON(t.type, t.features.values.toMutableList())
-                        geoJSON.value = converted
-                    }
+  fun getGeoJson() {
+    disposer.add(
+      fbInteractor.getGeoJson().subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeWith(object : DisposableObserver<FBGeoJSON>() {
+          override fun onNext(t: FBGeoJSON) {
+            val converted = GeoJSON(t.type, t.features.values.toMutableList())
+            geoJSON.value = converted
+          }
 
-                    override fun onError(e: Throwable) {
-                        throw e
-                    }
+          override fun onError(e: Throwable) {
+            throw e
+          }
 
-                    override fun onComplete() {
-                        TODO("Not yet implemented")
-                    }
-                })
-        )
-    }
-    fun addPointToDB(point: Point) {
-        disposer.add(
-            fbInteractor.addFeatureToDB(point).subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribeWith(object : DisposableSingleObserver<Point>(){
-                    override fun onSuccess(t: Point) {
-                        Logger.d("Added feature: %s", t)
-                    }
+          override fun onComplete() {
+            TODO("Not yet implemented")
+          }
+        })
+    )
+  }
 
-                    override fun onError(e: Throwable) {
-                        throw e
-                    }
+  fun addPointToDB(point: Point) {
+    disposer.add(
+      fbInteractor.addFeatureToDB(point).subscribeOn(Schedulers.io())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribeWith(object : DisposableSingleObserver<Point>() {
+          override fun onSuccess(t: Point) {
+            Logger.d("Added feature: %s", t)
+          }
 
-                })
-        )
-    }
+          override fun onError(e: Throwable) {
+            throw e
+          }
+
+        })
+    )
+  }
 }
